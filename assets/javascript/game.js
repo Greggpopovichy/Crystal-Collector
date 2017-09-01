2f2f2013044dcd1c75d76a9b6dd59b118d76b8dd
Charlie Arrendale <carrendale@gmail.com>
2017-09-01 15:09:26 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,23 @@
-$(document).ready(function){
+$(document).ready(function(){
 
-    //create a random number to append to #counter element. This number will be between 19 and 120
-    var random = Math.floor(Math.random()*101 +19)
-    $("#computerNumber").text(random);
+    //creating a random number to write into #computerNumber class in html. This number will be between 19 and 120
+    $(document).on("load", function(random){
+        var random = Math.floor(Math.random() * 101 + 19);
+        $("#computerNumber").html(random);
+    });
 
-    //setting variables for crystals to hold random numbers (1-12)
+    //setting variables for crystals to hold random numbers (1-12). Using Math.random to generate number
     var crystal1 = Math.floor(Math.random() * 11+1);
     var crystal2 = Math.floor(Math.random() * 11+1);
     var crystal3 = Math.floor(Math.random() * 11+1);
     var crystal4 = Math.floor(Math.random() * 11+1);
 
-    //setting variables to keep track of user data
+    //initializing variables to keep track of user data
     var userScore = 0;
     var wins = 0;
     var losses = 0;
 
-    //displays counter for wins and losses in #wins and #losses ids within HTML doc
+    //displays count for wins and losses in #wins and #losses ids within HTML doc
     $("#wins").text(wins);
     $("#losses").text(losses);
 
@@ -37,8 +39,8 @@ $(document).ready(function){
 $(".button1").on('click', function(){
     userScore = userScore + crystal1;
     console.log("New userScore= " + userScore);
-
     $(".total_score").text(userScore);
+
         if(userScore === random){
             winner();
         }
@@ -46,6 +48,7 @@ $(".button1").on('click', function(){
             loser();
         }
     });
+
 $(".button2").on('click', function(){
         userScore = userScore + crystal2;
         console.log("New userScore= " + userScore);
@@ -58,6 +61,7 @@ $(".button2").on('click', function(){
             loser();
         }
     });
+
 (".button3").on('click', function(){
         userScore = userScore + crystal3;
         console.log("New userScore= " + userScore);
@@ -83,6 +87,4 @@ $(".button2").on('click', function(){
             loser();
         }
     });
-
-
 });
